b3901eb4ba87d994512c6d92496b8286bf90fc6c
JunnielRome <60454482+JunnielRome@users.noreply.github.com>
2021-05-17 21:12:21 +0800

    Add files via upload

---
+++ signup-login/main.js
@@ -0,0 +1,8 @@
+function showLetters() {
+    var x = document.getElementById("password");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  }
