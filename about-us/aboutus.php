ab2d806b3275d3b070f8d0af501087fb233967f4
Fourthram Kaimo <frkaimo@up.edu.ph>
2021-06-26 20:58:02 +0800

    added profile page and edited about us page

--- about-us/aboutus.php
+++ about-us/aboutus.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>About us | Plantita ph</title>
     <script src="https://kit.fontawesome.com/6f5f971efa.js" crossorigin="anonymous"></script>
+    <title>About Us | Plantita ph</title>
     <link rel="stylesheet" href="../navbar/style.css">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="aboutus.css">
 </head>
 <body>
     <div class="container-wrapper">
@@ -15,17 +15,60 @@
             include_once '../navbar/navbar.php';
         ?>
         <div class="sections">
-            <div class="main-contents">
-                <div class="header">
-                    <h1>“Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium”</h1>
+            <section class="landing-page">
+                <div class="main-landing">
+                    <div class="hero-headlines">
+                        <div class="third-page-bg left-side">
+                            <img src="../homepage/images/third-page-bg.png" alt="background leaves">
+                        </div>
+                        <div class="third-page-bg right-side">
+                            <img src="../homepage/images/bg-plant-picture-right.png" alt="background leaves">
+                        </div>
+                        <h1>“Plant love, let it sprout.”</h1><h3>― H.S. Crow</h3>
+                        <br>
+                        <h2>Welcome to <span>plantita.ph</span></h2>
+                        <p>We aim to offer our customers a variety of the latest homeplants, outdoor plants, and secculents. 
+                        We’ve come a long way, so we know exactly which direction to take when supplying you with high quality yet budget-friendly products. 
+                        We offer all of this while providing excellent customer service and friendly support.
+                        We always keep an eye on the latest trends in plants and put our customers’ wishes first. 
+                        That is why we have satisfied customers all over the world, and are thrilled to be a part of the plant industry.</p>
+                        <p>In  today’s  market  trend,  going  green  is  the  “way”. People often engage themselves with plants as their new hobby, especially during this seem-to-never-end quarantine. 
+                        When it comes to buying plants, it is indeed the  greatest  task  for  consumers  to  know  where  to  buy plants that they want. 
+                        In some cases, people just want to buy plants that suit their lifestyle, but they mostly do not know what plants are going to meet their wants.</p>
+                        <p>The interests of our customers are always top priority for us, so we hope you will enjoy our products as much as we enjoy making them available to you.</p>
+                    </div>
                 </div>
-                <div class="content">
-                    <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-
-                    <p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt."</p>
+            </section>
+            <section class="third-page">
+                <div class="third-page-container">
+                    <div class="third-page-heading">
+                        <h1>Meet the Team</h1>
+                    </div>
+                    <div class="team-content">
+                    <!--insert img src for your pictures-->
+                        <div class="team-1 team">
+                            <img src="# alt="junniel" class="top-image">
+                            <h2 class="team-name">Junniel Rome A. Ardepuela</h2>
+                            <p class="position">President <br> Chief Executive Officer</p>
+                        </div>
+                        <div class="team-2 team">
+                            <img src="#" alt="dani" class="top-image">
+                            <h2 class="team-name">Daniel Gabriel S. Manabat</h2>
+                            <p class="position">Chief Operating Officer <br> Chief Financial Officer</p>
+                        </div>
+                        <div class="team-3 team">
+                            <img src="images/port.jpg" alt="fourthram" class="top-image">
+                            <h2 class="team-name">Fourthram R. Kaimo</h2>
+                            <p class="position">Senior Vice President<br> Customer Experience</p>
+                        </div>
+                        <div class="team-4 team">
+                            <img src="#" alt="ron" class="top-image">
+                            <h2 class="team-name">Ron Bryan M. Vertudes</h2>
+                            <p class="position">Chief of Staff Office of the CEO</p>
+                        </div>
+                    </div>
                 </div>
-                <a href="../catalogue/catalogue.php" class="btn">View plants</a>
-            </div>
+            </section>
             <footer>
                 <div class="footer-container">
                     <div class="footer-logo">
@@ -42,7 +85,7 @@
                     </div>
                 </div>      
             </footer>
-        </div>
+        </div>        
     </div>
     <script src="../navbar/app.js"></script>
 </body>
