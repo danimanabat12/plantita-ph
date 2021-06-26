fc38686853ea996d2e5d811771ef9d956ff27ad5
daniel manabat <dsmanabat@up.edu.ph>
2021-06-27 05:46:20 +0800

    added contact us with style revision

--- contact-us/contact.php
+++ contact-us/contact.php
@@ -1,98 +1,99 @@
+<?php //di ko sure asay tama aning duha
+        // //connection of the server
+        // $con = mysqli_connect('localhost', 'root', '', 'plantita_ph');
+        
+        // //the connection is unsuccessful
+        // if(!$con){
+        //     echo '<script type="text/javascript">';
+        //     echo ' alert("There is a connection error.")';  //showing an alert box.
+        //     echo '</script>';
+            
+        //     header("Location: signup.php");
+        //     exit();
+        // }
+
+        // if(isset($_POST['submit'])){
+        //     $name = mysqli_real_escape_string($con, $_POST['name']);
+        //     $email = mysqli_real_escape_string($con, $_POST['email']);
+        //     $msg = mysqli_real_escape_string($con, $_POST['message']);
+
+        //     $sql = "SELECT * FROM partners WHERE name = '$name'";
+        //     $query = mysqli_query($con, $sql) or die(mysqli_error($con));
+            
+        //     if (mysqli_num_rows($res_u) == 1) {    
+        //         $insert = "INSERT INTO partners (name, email, message) VALUES('$name', '$email', '$msg')";
+        //         $result = mysqli_query($con, $insert) or die(mysqli_error($con));
+        //         $message = 'Thank you for your collaboration!';
+        //     }
+        // }
+
+        // //close
+        // mysqli_close($con);
+
+			//start the connection of the server
+      include_once '../inc/config.db.php';
+
+      //session_start(); //start the session of the server
+			
+      //this function will validate the input
+			function validate($input){
+				$input = trim($input); 
+				$input = stripslashes($input);
+				$input = htmlspecialchars($input);
+				
+				return $input;            
+			}
+
+			if(isset($_POST['submit'])){
+				$name = validate($_POST["name"]);    //POST function to get what was the input for the username
+				$email = validate($_POST["email"]);            //POST function to get what was the input for the email address
+				$msg= validate($_POST["message"]);    //POST function to get what was the input for the password
+				
+				$sql_u = "SELECT * FROM users WHERE username='$name'";
+				//$sql_e = "SELECT * FROM users WHERE email='$txtEmail'";
+        //$sql_p = "SELECT * FROM users WHERE password='$txtPassword'";
+				$res_u = mysqli_query($con, $sql_u) or die(mysqli_error($con));
+				//$res_e = mysqli_query($con, $sql_e) or die(mysqli_error($con));
+
+        //$hash = password_hash($sql_p, PASSWORD_DEFAULT);
+
+				
+					$query_1 = "INSERT INTO partners (name, email, message)
+							VALUES ('$name', '$email', '$msg')";
+					$result = mysqli_query($con, $query_1) or die(mysqli_error($con));
+                    $message = 'Thank you for your collaborations!';
+                
+			}
+
+    // Close the connection
+    mysqli_close($con);
+	
+    ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en"> 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Contact Us | Plantita ph</title>
     <script src="https://kit.fontawesome.com/6f5f971efa.js" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <link rel="stylesheet" href="../navbar/style.css">
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <div class="container-wrapper">
-        <!-- <nav>
-            <div class="outer-shell">
-                <div class="logo">
-                    <a href="../homepage/index.html"><img src="../navbar/nav-logo.svg" alt="logo"></a>
-                </div>     
-                <div class="burger">
-                    <div class="line1"></div>
-                    <div class="line2"></div>
-                    <div class="line3"></div>
-                </div>
-                <div class="cart">
-                    <img src="../navbar/shopping-cart.png" alt="cart">
-                    <p id="no-of-items">0</p>
-                </div>
-            </div>   
-            <div class="inner-shell">
-                <div class="top-part">
-                    <div class="logo2">
-                        <img src="../navbar/nav-logo.svg" alt="logo">
-                        <p><a href="../homepage/index.html">Plantita.ph</a></p>
-                    </div>
-                </div>
-                <div class="middle-part">
-                    <ul class="nav-links">
-                        <li>
-                            <h1><a href="../homepage/index.html">home</a></h1>
-                        </li>
-                        <li class="catalogue-container">
-                            <h1><a href="../catalogue/index.html">catalogue</a></h1>
-                            <p><a href="../catalogue/index.html">homeplants</a></p>
-                            <p><a href="../catalogue/index.html">outdoor plants</a></p>   
-                            <p><a href="../catalogue/index.html">succulents</a></p>
-                        </li>
-                        <li>
-                            <h1><a href="../about-us/aboutus.html">about</a></h1>
-                        </li>
-                        <li>
-                            <h1><a href="../filler_page/404page.html">guides</a></h1>
-                        </li>
-                        <li class="sublinks">
-                            <p><a href="../signup-login/login.html">sign in</a></p>
-                            <p class="current"><a href="#">contact us</a></p>
-                            <p><a href="../filler_page/404page.html">policies</a></p>
-                            <p><a href="../filler_page/404page.html">help</a></p>
-                        </li>
-                    </ul>
-                    <div class="burger-inside">
-                        <div class="line1"></div>
-                        <div class="line2"></div>
-                        <div class="line3"></div>
-                    </div>
-                </div>
-                <div class="cart-inside">
-                    <img src="../navbar/shopping-cart.png" alt="cart">
-                    <p id="no-of-items">0</p>
-                </div>
-            </div>
-            <div class="shopping-cart">
-                <div class="top-part">
-                    <div class="logo2">
-                        <img src="../navbar/nav-logo.svg" alt="logo">
-                        <p><a href="../homepage/index.html">Plantita.ph</a></p>
-                    </div>
-                    <p class="closing-btn">close</p>
-                </div>
-                <div class="sc-middle-part">
-                    <h1 class="sc-title">My Shopping Cart</h1>
-                    <div class="shopping-cart-items">
-                        <ul class="shopping-list">
-                        </ul>
-                    </div>
-                </div>
-                <div class="cart">
-                </div>
-            </div>    
-        </nav> -->
         <?php
             include_once '../navbar/navbar.php';
         ?>
         <div class="sections">
             <div class="main-content-container">
                 <h1>Reach out</h1>
+                <?php 
+                    if(isset($message)){
+                        echo  '<label style= "color: #164a41; text-align: center; margin-bottom: 2rem; font-size: 2rem;">'.$message. '</label>';
+                    }
+                ?>
                 <div class="second-part" >
                     <div class="sp-title">
                         <h2>Customer Service</h2>
@@ -102,24 +103,27 @@
                         <h2>General Inquiries</h2>
                         <p>hello@plantita.ph</p>
                     </div>
+                    <div class="sp-title">
+                        <h2>General Inquiries</h2>
+                        <p>hello@plantita.ph</p>
+                    </div>
                 </div>
                 <div class="third-part">
-                    <h2>Or get in touch with the form below:</h2>
-                    <form action="#">
+                    <h1>Or get in touch with the form below</h1>
+                    <form action="contact.php" method="post">
                         <div class="style-input">
-                            <label for="name">Name</label>
-                            <input type="text" name="name" id="cntct-name" required>
+                            <input type="text" name="name" id="cntct-name" placeholder="Name" required>
                         </div>
                         <div class="style-input">
-                            <label for="email">Email</label>
-                            <input type="email" name="email" id="cntct-email" required>
+                            <!-- <label for="email">Email</label> -->
+                            <input type="email" name="email" id="cntct-email" placeholder="Email" required>
                         </div>
                         <div class="style-input">
-                            <label for="message">Message</label>
-                            <textarea name="message" id="cntct-message" cols="50" rows="3" required></textarea>
+                            <!-- <label for="message">Message</label> -->
+                            <textarea name="message" id="cntct-message" placeholder="Message" class="input-full" required></textarea>
                         </div>
-                        <input type="submit" value="Submit" class="btn-solid">
-                        <!-- <button type="submit" class="btn-solid">Submit</button> -->
+                        <input name="submit" type="submit" value="Submit" class="btn-solid">
+                        <!-- <button  name="submit"  type="submit" class="btn-solid">Submit</button> -->
                     </form>
                 </div>
             </div>
